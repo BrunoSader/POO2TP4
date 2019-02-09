400b2fe03bbe493e9f32edaedbda9119fdbf7c87
BrunoSader <brunosader@hotmail.com>
2019-02-09 17:07:54 +0100

    Ajout des classes graphGen

---
+++ graphGen.h
@@ -0,0 +1,92 @@
+/*************************************************************************
+                           graphGen  -  description
+                             -------------------
+    d�but                : ${date}
+    copyright            : (C) ${year} par ${user}
+*************************************************************************/
+
+//---------- Interface de la classe <graphGen> (fichier graphGen.cpp) ------
+#if ! defined ( GRAPHGEN_H )
+#define GRAPHGEN_H
+
+//--------------------------------------------------- Interfaces utilis�es
+#include <iostream>
+#include <unordered_map>
+#include <map>
+#include "reader.h"
+using namespace std;
+//------------------------------------------------------------- Constantes
+
+//------------------------------------------------------------------ Types
+
+//------------------------------------------------------------------------
+// R�le de la classe <graphGen>
+//
+//
+//------------------------------------------------------------------------
+
+class graphGen
+{
+//----------------------------------------------------------------- PUBLIC
+
+public:
+//----------------------------------------------------- M�thodes publiques
+    // type M�thode ( liste de param�tres );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+
+//------------------------------------------------- Surcharge d'op�rateurs
+    graphGen & operator = ( const graphGen & ungraphGen );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+
+//-------------------------------------------- Constructeurs - destructeur
+    graphGen ( const graphGen & ungraphGen );
+    // Mode d'emploi (constructeur de copie) :
+    //
+    // Contrat :
+    //
+
+    graphGen ( );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+    virtual ~graphGen ( );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+//------------------------------------------------------------------ PRIVE
+
+protected:
+//----------------------------------------------------- M�thodes prot�g�es
+
+private:
+//------------------------------------------------------- M�thodes priv�es
+
+protected:
+//----------------------------------------------------- Attributs prot�g�s
+
+private:
+//------------------------------------------------------- Attributs priv�s
+
+//---------------------------------------------------------- Classes amies
+
+//-------------------------------------------------------- Classes priv�es
+
+//----------------------------------------------------------- Types priv�s
+
+};
+
+//----------------------------------------- Types d�pendants de <graphGen>
+
+#endif // GRAPHGEN_H
--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ int main(int argc, char **argv){
     while ((opt = getopt(argc,argv,"g:et:")) != EOF)
         switch(opt)
         {
-            case 'g': 
+            case 'g':
                 ss<<optarg;
                 ss>>nomFichierGraph;
                 ss.clear();
@@ -32,44 +32,43 @@ int main(int argc, char **argv){
                     cerr<<"the hour should be a number between 0 and 23"<<endl;
                     erreur=true;
                     break;
-				}else
-				{
+								}else
+								{
                 option+=1;
                 break;
-				}
-            case 'e': 
+								}
+            case 'e':
                 cout << "e is enabled" <<endl;
                 option+=10;
                 break;
-            case '?': 
+            case '?':
                 erreur=true;
                 break;
-            default: 
-                cout<<"d"<<endl; 
+            default:
+                cout<<"d"<<endl;
         }
     cout<<"option = "<<option<<endl;
     string nom(argv[argc-1]);
     if(!erreur)
     {
-		reader r =reader(nom);
-		if(option == 1)
-		{
-			top10 t = top10(r,heure);
-			cout << t;
-		}else if(option == 10)
-		{
-			top10 t = top10(r,24,"e");
-			cout << t;
-		}else if(option == 11)
-		{
-			top10 t = top10(r,heure,"e");
-			cout << t;
-		}else
-		{
-			top10 t = top10(r);
-			cout << t;
-		}
-		
+			reader r =reader(nom);
+			if(option == 1)
+			{
+				top10 t = top10(r,heure);
+				cout << t;
+			}else if(option == 10)
+			{
+				top10 t = top10(r,24,"e");
+				cout << t;
+			}else if(option == 11)
+			{
+				top10 t = top10(r,heure,"e");
+				cout << t;
+			}else
+			{
+				top10 t = top10(r);
+				cout << t;
+			}
     }
     return 0;
 }
